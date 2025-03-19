23e73879964936ebc8a94242e33a6da88ef4f166
ZakiaRamzan <141837084+ZakiaRamzan@users.noreply.github.com>
2025-03-19 21:45:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+  let display = document.getElementById("display");
+  function clearDisplay(){
+    display.value = "";
+  }
+  function deleteLast(){
+    display.value= display.value.slice(0,-1);
+  }
+  function appendValue(value){
+    display.value += value;
+  }
+  function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch(error){
+        display.value = "Error"
+    }
+  }
